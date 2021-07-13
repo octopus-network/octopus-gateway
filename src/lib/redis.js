e60a690e0e1992c0c905d5adffbaca5596e520b1
ruanyu <>
2021-07-13 13:43:10 +0800

    use Memorystore for Redis with auth/tls enabled

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,10 +1,14 @@
+const fs = require('fs')
 const { logger } = require('./log')
 var Redis = require('ioredis')
 
 client = new Redis({
     port: config.redis.port,
     host: config.redis.host,
-    password: config.redis.password
+    password: process.env.REDIS_PASSWORD || config.redis.password,
+    tls: {
+        ca: process.env.REDIS_TLS_CRT || fs.readFileSync(config.redis.cert),
+    },
 })
 client.on("error", function (error) {
     logger.error('redis error', error)
