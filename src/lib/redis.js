a3a1f95d486080784948caaac4fe871186f2fb64
ruanyu <>
2021-07-14 15:45:39 +0800

    redis config from process.env

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -3,8 +3,8 @@ const { logger } = require('./log')
 var Redis = require('ioredis')
 
 client = new Redis({
-    port: config.redis.port,
-    host: config.redis.host,
+    host: process.env.REDIS_HOST || config.redis.host,
+    port: process.env.REDIS_PORT || config.redis.port,
     password: process.env.REDIS_PASSWORD || config.redis.password,
     tls: {
         ca: process.env.REDIS_TLS_CRT || fs.readFileSync(config.redis.cert),
