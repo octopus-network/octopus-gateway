2c0a625cc5246a5d04b08f0c6c9016e532bef538
ruanyu <>
2022-03-24 20:10:28 +0800

    write to bq

--- stats/hourly_api_stats.py
+++ stats/hourly_api_stats.py
@@ -19,8 +19,6 @@ import argparse
 import json
 import logging
 import sys
-import time
-from datetime import datetime
 from typing import Optional, Tuple, Dict, Union
 
 import apache_beam as beam
@@ -35,10 +33,26 @@ from apache_beam.transforms.trigger import AfterWatermark
 from apache_beam.transforms.trigger import AccumulationMode
 from apache_beam.transforms.window import FixedWindows
 from apache_beam.transforms.window import TimestampedValue
+from apache_beam.utils.timestamp import Timestamp
 
 
-def timestamp2str(t, fmt='%Y-%m-%d %H:%M:%S.000'):
-    return datetime.fromtimestamp(t).strftime(fmt)
+class Method(object):
+    def __init__(self, chain, project, method):
+        self.chain = chain
+        self.project = project
+        self.method = method
+
+
+class MethodCoder(beam.coders.Coder):
+    def encode(self, v):
+        return f"{v.chain}:{v.project}:{v.method}".encode('utf-8')
+
+    def decode(self, e):
+        v = e.decode('utf-8').split(':')
+        return Method(*v)
+
+    def is_deterministic(self):
+        return True
 
 
 class ParseJsonRpcFn(beam.DoFn):
@@ -71,42 +85,22 @@ class ParseJsonRpcFn(beam.DoFn):
         logging.error('Parse error on "%s"', elem)
 
 
-class Method(object):
-    def __init__(self, chain, project, method):
-        self.chain = chain
-        self.project = project
-        self.method = method
-
-
-class MethodCoder(beam.coders.Coder):
-    def encode(self, m):
-        return f"{m.name}:{m.project}:{m.method}".encode('utf-8')
-
-    def decode(self, s):
-        m = s.decode('utf-8').split(':')
-        return Method(*m)
-
-    def is_deterministic(self):
-        return True
-
-
 @beam.typehints.with_output_types(Tuple[Method, Tuple[str, float, int, Optional[int]]])
 def GetMethodFn(elem):
-    print("????", elem)
     method = Method(elem['chain'], elem['project'], elem['method'])
     others = (elem['type'], elem['duration'], elem['length'], elem.get('error'))
     return method, others
 
 
 @beam.typehints.with_input_types(Tuple[str, float, int, Optional[int]])
-@beam.typehints.with_output_types(Tuple[int, int, int, float, float])
+@beam.typehints.with_output_types(Tuple[int, int, int, float, int])
 class CombineMethodFn(beam.CombineFn):
     def create_accumulator(self):
         category = 1 # 1-req 2-sub
         count = 0
+        errors = 0
         duration = .0
         length = 0
-        errors = 0
         accumulator = category, count, errors, duration, length
         return accumulator
 
@@ -123,17 +117,17 @@ class CombineMethodFn(beam.CombineFn):
 
     def extract_output(self, accumulator):
         category, count, errors, duration, length = accumulator
-        mean_duration = float('NaN')
-        if category == 1:
-            mean_duration = (float('NaN') if count == 0 else duration/count)
-        mean_length = (float('NaN') if count == 0 else length/count)
-        return category, count, errors, mean_duration, mean_length
+        # mean_duration = float('NaN')
+        # if category == 1:
+        #     mean_duration = (float('NaN') if count == 0 else duration/count)
+        return category, count, errors, duration, length
 
 
-@beam.typehints.with_input_types(Tuple[Method, Tuple[int, int, int, float, float]])
+@beam.typehints.with_input_types(Tuple[Method, Tuple[int, int, int, float, int]])
 @beam.typehints.with_output_types(Dict[str, Union[str, int, float]])
 class ConvertToDict(beam.DoFn):
     def process(self, elem, window=beam.DoFn.WindowParam):
+        ts_format = '%Y-%m-%d %H:%M:%S.%f UTC'
         method, stats = elem
         yield {
             'chain': method.chain,
@@ -142,11 +136,28 @@ class ConvertToDict(beam.DoFn):
             'category': stats[0],
             'count': stats[1],
             'errors': stats[2],
-            'mean_duration': stats[3],
-            'mean_length': stats[4],
-            'window_start': timestamp2str(int(window.start)),
-            'processing_time': timestamp2str(int(time.time()))
+            'duration': stats[3],
+            'length': stats[4],
+            'window_start': window.start.to_utc_datetime().strftime(ts_format),
+            'processing_time': Timestamp.now().to_utc_datetime().strftime(ts_format)
         }
+
+
+class CalculateMethodStats(beam.PTransform):
+    def __init__(self, window_duration, allowed_lateness):
+        self.window_duration = window_duration * 60
+        self.allowed_lateness_seconds = allowed_lateness * 60
+
+    def expand(self, pcoll):
+        return (
+            pcoll
+            | 'FixedWindows' >> beam.WindowInto(FixedWindows(self.window_duration),
+                trigger=AfterWatermark(early=AfterProcessingTime(10), late=AfterCount(1)),
+                accumulation_mode=AccumulationMode.ACCUMULATING,
+                allowed_lateness=self.allowed_lateness_seconds)
+            | 'GetMethod' >> beam.Map(GetMethodFn)
+            | 'CombineMethod' >> beam.CombinePerKey(CombineMethodFn())
+        )
 
 
 class WriteToBigQuery(beam.PTransform):
@@ -163,23 +174,7 @@ class WriteToBigQuery(beam.PTransform):
         return (
             pcoll
             | 'ConvertToRow' >> beam.Map(lambda elem: {col: elem[col] for col in self.schema})
-            | beam.io.WriteToBigQuery(self.table_name, self.dataset, self.project, self.get_schema()))
-
-
-class CalculateMethodStats(beam.PTransform):
-    def __init__(self, window_duration, allowed_lateness):
-        self.window_duration = window_duration * 60
-        self.allowed_lateness_seconds = allowed_lateness * 60
-
-    def expand(self, pcoll):
-        return (
-            pcoll
-            | 'FixedWindows' >> beam.WindowInto(FixedWindows(self.window_duration),
-                trigger=AfterWatermark(early=AfterProcessingTime(60), late=AfterCount(1)),
-                accumulation_mode=AccumulationMode.ACCUMULATING,
-                allowed_lateness=self.allowed_lateness_seconds)
-            | 'GetMethod' >> beam.Map(GetMethodFn)
-            | 'CombineMethod' >> beam.CombinePerKey(CombineMethodFn())
+            | beam.io.WriteToBigQuery(self.table_name, self.dataset, self.project, self.get_schema())
         )
 
 
@@ -198,7 +193,7 @@ def run(argv=None, save_main_session=True):
         help='BigQuery Dataset to write tables to. Must already exist.')
     parser.add_argument(
         '--table_name',
-        default='gateway_stats',
+        default='stats',
         help='The BigQuery table name. Should not already exist.')
     parser.add_argument(
         '--window_duration',
@@ -237,7 +232,7 @@ def run(argv=None, save_main_session=True):
             | 'CalcMethodStats' >> CalculateMethodStats(args.window_duration, args.allowed_lateness)
             | 'ConvertToDict' >> beam.ParDo(ConvertToDict())
             | 'WriteMethodStats' >> WriteToBigQuery(
-                args.table_name + '_method',
+                args.table_name + '_method_hourly',
                 args.dataset,
                 {
                     'chain': 'STRING',
@@ -246,10 +241,10 @@ def run(argv=None, save_main_session=True):
                     'category': 'INTEGER',
                     'count': 'INTEGER',
                     'errors': 'INTEGER',
-                    'mean_duration': 'FLOAT',
-                    'mean_length': 'FLOAT',
-                    'window_start': 'STRING',
-                    'processing_time': 'STRING'
+                    'duration': 'FLOAT',
+                    'length': 'INTEGER',
+                    'window_start': 'TIMESTAMP',
+                    'processing_time': 'TIMESTAMP'
                 },
                 options.view_as(GoogleCloudOptions).project)
         )
